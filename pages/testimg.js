b122c92aa40dc4a4bae609e77b7de588fd7919b4
Naweeparb Wangwanich <n.wangwanich@mail.com>
2021-04-07 14:35:43 +0700

    Take and Crop Picture

--- pages/testimg.js
+++ pages/testimg.js
@@ -1,4 +1,6 @@
-import * as canvas from 'canvas';
+// https://stackoverflow.com/questions/13073647/crop-canvas-export-html5-canvas-with-certain-width-and-height
+// https://stackoverflow.com/questions/26015497/how-to-resize-then-crop-an-image-with-canvas
+
 import * as faceapi from 'face-api.js';
 
 import { useState, useEffect } from 'react';
@@ -11,6 +13,7 @@ const Index = () => {
     const [y, setY] = useState(0);
     const [w, setW] = useState(0);
     const [h, setH] = useState(0);
+    const [enable, setEnable] = useState(true);
     function Webcam() {
         Promise.all([
             faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
@@ -34,7 +37,7 @@ const Index = () => {
             e.preventDefault();
             var selection = document.querySelector('canvas') === null;
             var sel_z = selection.id === 'test';
-            console.log(sel_z)
+            // console.log(sel_z)
             if (selection) {
                 const canvas = faceapi.createCanvasFromMedia(video)
                 canvas.id = 'test'
@@ -44,18 +47,27 @@ const Index = () => {
                 setInterval(async () => {
                     const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
                     const resizedDetections = faceapi.resizeResults(detections, displaySize)
+                    // console.log(detections);
+                    // console.log(resizedDetections);
                     canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
                     canvas.getContext('2d').drawImage(video,0,0)
                     faceapi.draw.drawDetections(canvas, resizedDetections)
                     // faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
                     faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
                     if (resizedDetections[0] !== undefined){
-                        // console.log(resizedDetections[0]['detection']['_box']['_x'])
-                        setX(resizedDetections[0]['detection']['_box']['_x'])
-                        setY(resizedDetections[0]['detection']['_box']['_y'])
-                        setW(resizedDetections[0]['detection']['_box']['_width'])
-                        setH(resizedDetections[0]['detection']['_box']['_height'])
-                        // document.querySelector('button').setAttribute()
+                        setX(resizedDetections[0]['detection']['_box']['_x']);
+                        setY(resizedDetections[0]['detection']['_box']['_y']);
+                        setW(resizedDetections[0]['detection']['_box']['_width']);
+                        setH(resizedDetections[0]['detection']['_box']['_height']);
+
+                        var x = resizedDetections[0]['detection']['_score']
+                        if (x >= 0.9){
+                            // document.getElementById('takephoto').disabled = false;
+                            setEnable(false);
+                        }
+                    }else{
+                        // document.getElementById('takephoto').disabled = true;
+                        setEnable(true);
                     }
                 }, 100)
             }
@@ -66,7 +78,12 @@ const Index = () => {
         <div id='main'>
                 <video id="video" height="500" width="500" autoPlay muted />
                 <br/>
-                <button onClick={()=> takePhoto('test',x,y,w,h)}>take Photo</button>
+                <button
+                    id = "takephoto"
+                    disabled = {enable}
+                    onClick={()=> takePhoto('test',x,y,w,h)}>
+                    Take Photo
+                </button>
                 <br/>
         </div>
     )
@@ -81,6 +98,19 @@ function takePhoto(x_val,c_x,c_y,c_w,c_h){
     // img.src = data;
     // var src = document.querySelector('body')
     // src.appendChild(img);
+
+    var canvas = document.getElementById(x_val)
+    var tempCanvas = document.createElement("canvas"),
+        tCtx = tempCanvas.getContext("2d");
+
+    tempCanvas.width = c_w;
+    tempCanvas.height = c_h;
+
+    tCtx.drawImage(canvas,c_x,c_y,c_w,c_h,0,0,c_w,c_h);
+
+    // write on screen
+    var img = tempCanvas.toDataURL("image/png");
+    document.write('<a href="'+img+'"><img src="'+img+'"/></a>');
 }
 
 export default Index;
